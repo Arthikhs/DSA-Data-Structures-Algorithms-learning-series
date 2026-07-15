0c532731d0b57c0180b929fef45fad778ecfc62c
Arthik H S <hsarthik5@gmail.com>
2026-07-15 23:06:02 +0530

    fix: updated env files for localhost

---
+++ Arrays/sqrt.java
@@ -0,0 +1,13 @@
+package Arrays;
+import java.util.Scanner;
+public class sqrt {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        if(n<2){
+            System.out.println(n);
+        }
+
+    }
+    
+}
